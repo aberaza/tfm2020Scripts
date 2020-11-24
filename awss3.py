4529d1eb35f69a73c17545fa79cbe6c8b889287b
Aritz Beraza <aritz.beraza@gmail.com>
2020-11-24 19:10:45 +0100

    awss3 proxy to work with aws S3

---
+++ awss3.py
@@ -0,0 +1,28 @@
+import boto3
+import pickle
+import pandas as pandas
+import io
+
+class S3proxy():
+  def __init__(self, bucket, key, pwd):
+    self.BUCKET_NAME = bucket
+    self.s3 = boto3.resource('s3',
+                    aws_access_key_id = key,
+                    aws_secret_access_key = pwd)
+    self.s3_bucket = self.s3.Bucket(bucket)
+
+
+  def writeFileToS3(self, content, filename):
+    self.s3.Object(BUCKET_NAME, filename).put(Body=content)
+
+  def writeDFToS3(self, ndf, filename):
+    pickle_buffer = io.BytesIO()
+    pickle.dump(ndf, pickle_buffer)
+    self.writeFileToS3(pickle_buffer.getvalue(), filename)
+
+  def readFileFromS3(self, filename):
+    return self.s3.Object(BUCKET_NAME, filename).get()['Body'].read()
+
+  def readDFFromS3(self, filename):
+    return pickle.load(io.BytesIO(self.readFileFromS3(CSV_FILENAME)))
+
